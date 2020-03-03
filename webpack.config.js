f26617c0a7bffc5174388557e73b429d5209d359
Christopher Fields <chrisjr06@gmail.com>
2020-03-03 12:12:08 -0600

    added babel-loader to use async/await

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 console.log('running webpack');
 
@@ -29,9 +30,21 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: ['file-loader'],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
     ],
   },
   plugins: [
+    new BundleAnalyzerPlugin(),
     new ManifestPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({ title: 'Output Management' }),
